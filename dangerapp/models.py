0da9cf626a5ac8500356d774fddceafbd228a67e
azimjaan21 <azimaxtamov2oo2@mail.ru>
2025-03-18 10:11:44 +0900

    danger zone (demo) project is done

--- dangerapp/models.py
+++ dangerapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class PolygonZone(models.Model):
+    name = models.CharField(max_length=100, default="Zone")
+    points = models.JSONField()  # Stores polygon points as JSON
+
